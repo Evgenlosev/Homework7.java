254d06f7faf10092f18f43de1bd0a837fa2a327d
Evgenlosev <evgenlosev@yandex.ru>
2021-04-25 15:15:37 +0700

    Homework1

---
+++ src/main/java/Java2/HomeWork5/HomeWork5.java
@@ -0,0 +1,65 @@
+package Java2.HomeWork5;
+
+import java.util.Arrays;
+
+public class HomeWork5 {
+    static final int size = 10000000;
+    static final int h = size / 2;
+    float[] arr = new float[size];
+
+    public static void main(String[] args)  {
+        float[] arr = new float[size];
+        method1(arr);
+        method2(arr);
+    }
+
+    public static void method1(float arr[]) {
+        for (int i = 0; i < size; i++) {
+            arr[i]= 1;
+        }
+        long a = System.currentTimeMillis();
+        for (int i = 0; i < size; i++) {
+            arr[i] = (float) (arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+        }
+        System.out.println("Время выполнения первого метода: " + (System.currentTimeMillis() - a));
+    }
+
+    public static void method2(float arr[]) {
+        float a1[] = new float[h];
+        float a2[] = new float[h];
+
+        for (int i = 0; i < size; i++) {
+            arr[i] = 1;
+        }
+        long a = System.currentTimeMillis();
+        System.arraycopy(arr, 0, a1, 0, h);
+        System.arraycopy(arr, h, a2, 0, h);
+        Thread thread1 = new Thread(() -> {
+            for (int i = 0; i < h; i++) {
+                a1[i] = (float) (a1[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+            }
+        });
+        Thread thread2 = new Thread(() -> {
+            int j = h;
+            for (int i = 0; i < h; i++) {
+                a2[i] = (float) (a2[i] * Math.sin(0.2f + j / 5) * Math.cos(0.2f + j / 5) * Math.cos(0.4f + j / 2));
+                j++;
+            }
+        });
+        thread1.start();
+        thread2.start();
+        try {
+            thread1.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        try {
+            thread2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.arraycopy(a1, 0, arr, 0, h);
+        System.arraycopy(a2, 0, arr, h, h);
+        System.out.println("Время выполнения второго метода: " + (System.currentTimeMillis() - a));
+    }
+}
